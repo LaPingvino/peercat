e66550b70fee40acf74eff04e7e1fd6fb00848ed
Joop Kiefte <ikojba@gmail.com>
2019-02-19 01:28:18 +0100

    Initial commit: working cat implementation

---
+++ main.go
@@ -0,0 +1,26 @@
+// Peercat is a tool for connecting random computers on the internet in a netcat like style
+// using a shared connection string.
+package main
+
+import (
+	"os"
+	"io"
+)
+
+func main() {
+	var in io.Reader
+	in = os.Stdin
+	out := os.Stdout
+	if len(os.Args) > 1 {
+		ins := make([]io.Reader, len(os.Args)-1)
+		for i, file := range os.Args[1:] {
+			f, err := os.Open(file)
+			if err != nil {
+				panic(err)
+			}
+			ins[i] = f
+		}
+		in = io.MultiReader(ins...)
+	}
+	io.Copy(out, in)
+}
